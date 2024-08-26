b85b8a3b7b9d1d667e69a4792f579a4bb4b925e9
Lucas Mendonça <lucas.c.santos@capgemini.com>
2024-08-26 17:39:16 -0300

    sameSite

--- backend/src/main/java/com/iaeluk/finance/config/SecurityConfig.java
+++ backend/src/main/java/com/iaeluk/finance/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -43,6 +44,7 @@ public class SecurityConfig {
 
                 )
                 .oauth2ResourceServer(rs -> rs.jwt(Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).sessionFixation().none())
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl(frontUrl)
