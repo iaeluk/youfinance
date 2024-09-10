72f3039292c23d39960b04e14153f3bc51aef060
Lucas Mendonça <lucas.c.santos@capgemini.com>
2024-09-10 12:21:30 -0300

    angular-oauth2-oidc

---
+++ backend/src/main/java/com/iaeluk/finance/config/UserCreationFilter.java
@@ -0,0 +1,28 @@
+package com.iaeluk.finance.config;
+
+import com.iaeluk.finance.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import com.iaeluk.finance.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOidcUserService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public OidcUser loadUser(OidcUser userRequest) throws OAuth2AuthenticationException {
+        OidcUser oidcUser = super.loadUset(userRequest);
+        userService.findOrCreateUser(oidcUser);
+        return oidcUser;
+    }
+
+}
